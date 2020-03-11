1ad3bb7c87f42c53727c48155b2b61a76719fe0f
rocio montero <monterorocios@gmail.com>
2020-03-11 18:54:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+def display_board(board)
+puts "| |"
+puts "------------"
+# puts " ", |,  " ", |, " "
+puts "------------"
+end
